60737ab8400b137ceafafa877717575f9eb35c00
Vitor Ferreira <vitorfer238@gmail.com>
2020-06-01 08:17:00 -0300

    organizing the project

---
+++ app/src/main/java/com/vitor238/newyorktimesbooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package com.vitor238.newyorktimesbooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vitor238.newyorktimesbooks.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
