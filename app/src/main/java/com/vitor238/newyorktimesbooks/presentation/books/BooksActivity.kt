bd4972ba7799e94b3a6c0b84653d6673262b2026
Vitor Ferreira <vitorfer238@gmail.com>
2020-06-02 08:34:22 -0300

    adding toolbar and recyclerview to the project

--- app/src/main/java/com/vitor238/newyorktimesbooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/vitor238/newyorktimesbooks/presentation/books/BooksActivity.kt
@@ -3,11 +3,15 @@ package com.vitor238.newyorktimesbooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vitor238.newyorktimesbooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
     }
 }
