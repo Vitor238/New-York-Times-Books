a2764191b977361e3b1cc5218f04209c083e97f2
Vitor Ferreira <vitorfer238@gmail.com>
2020-06-10 07:29:48 -0300

    Adding the ViewModel

--- app/src/main/java/com/vitor238/newyorktimesbooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/vitor238/newyorktimesbooks/presentation/books/BooksActivity.kt
@@ -2,10 +2,11 @@ package com.vitor238.newyorktimesbooks.presentation.books
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vitor238.newyorktimesbooks.R
-import com.vitor238.newyorktimesbooks.data.model.Book
 import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
@@ -17,18 +18,22 @@ class BooksActivity : AppCompatActivity() {
         toolbarMain.title = getString(R.string.books_title)
         setSupportActionBar(toolbarMain)
 
-        with(recyclerBooks) {
-            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
-            setHasFixedSize(true)
-            adapter = BooksAdapter(getBooks())
-        }
+        val viewModel: BooksViewModel = ViewModelProviders.of(this).get(BooksViewModel::class.java)
+
+        viewModel.booksLiveData.observe(this, Observer {
+            it?.let { books ->
+                with(recyclerBooks) {
+                    layoutManager =
+                        LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+                    setHasFixedSize(true)
+                    adapter = BooksAdapter(books)
+                }
+            }
+        })
+
+
+        viewModel.getBooks()
     }
 
-    fun getBooks(): List<Book> {
-        return listOf(
-            Book("Title 1", "Author 1"),
-            Book("Title 2", "Author 2"),
-            Book("Title 3", "Author 3")
-        )
-    }
+
 }
